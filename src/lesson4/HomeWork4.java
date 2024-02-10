e6f7c2308ca8986782c28423b05bb7f1b38b0e6b
AlexPostica <145809454+AlexPostica@users.noreply.github.com>
2024-02-10 18:34:28 +0100

    Update HomeWork4.java

--- src/lesson4/HomeWork4.java
+++ src/lesson4/HomeWork4.java
@@ -1,4 +1,7 @@
 package lesson4;
+
+import java.util.Scanner;
+
 /**
  * AIT-TR, cohort 42.1, Java Basic, hm #4
  * @author Alexandru
@@ -6,4 +9,18 @@ package lesson4;
  */
 public class HomeWork4 {
 
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your name:");
+        String name = scanner.nextLine();
+        System.out.println("Enter your first name:");
+        String firstName = scanner.nextLine();
+        System.out.println("Enter your city:");
+        String city = scanner.nextLine();
+        System.out.println("Enter how many years you have:");
+        String age = scanner.nextLine();
+        System.out.println("Enter your height (meters):");
+        String height = scanner.nextLine();
+        System.out.println("My name " + name + ", i have " + age + " years," + " my city is " + city + ", my height " + height);
+    }
 }
