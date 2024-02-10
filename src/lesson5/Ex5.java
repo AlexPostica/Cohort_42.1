e26150fac5c2dea6b93a873ef7829d9be9ae504e
AlexPostica <145809454+AlexPostica@users.noreply.github.com>
2024-02-10 19:12:11 +0100

    Create Ex5.java

---
+++ src/lesson5/Ex5.java
@@ -0,0 +1,17 @@
+package lesson5;
+/**
+ * AIT-TR, cohort 42.1, Java Basic, #5
+ * @author Alexandru
+ * @version 23.Jan
+ */
+public class Ex5 {
+    public static void main(String[] args) {
+        int t = 29;
+//        if (t > 28) {
+//            System.out.println("It's hot");
+//        } else {
+//            System.out.println("It's NOT hot");
+//        }
+        System.out.println((t > 28)? "It's hot" : "It's NOT hot");
+    }
+}
