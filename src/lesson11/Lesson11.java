e10f05db894ccdc33948c7859b85b56c60bac7a8
AlexPostica <145809454+AlexPostica@users.noreply.github.com>
2024-02-10 21:40:22 +0100

    Update Lesson11.java

--- src/lesson11/Lesson11.java
+++ src/lesson11/Lesson11.java
@@ -100,15 +100,7 @@ public class Lesson11 {
                 return true;
             }
         }
-//        by x
-//        if (table[0][0] == chr && table[0][1] == chr && table[0][2] == chr) return true;
-//        if (table[1][0] == chr && table[1][1] == chr && table[1][2] == chr) return true;
-//        if (table[2][0] == chr && table[2][1] == chr && table[2][2] == chr) return true;
-//        // by y
-//        if (table[0][0] == chr && table[1][0] == chr && table[2][0] == chr) return true;
-//        if (table[0][1] == chr && table[1][1] == chr && table[2][1] == chr) return true;
-//        if (table[0][2] == chr && table[1][2] == chr && table[2][2] == chr) return true;
-
+        
         // diagonals
         if ((table[0][0] == chr && table[1][1] == chr && table[2][2] == chr)
             || (table[0][2] == chr && table[1][1] == chr && table[2][0] == chr)) {
