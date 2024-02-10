592e248183beb65693e04080b948b0558d33a910
AlexPostica <145809454+AlexPostica@users.noreply.github.com>
2024-02-10 21:24:40 +0100

    Create Interpreter.java

---
+++ src/extend/Interpreter.java
@@ -0,0 +1,33 @@
+package extend;
+
+import java.util.Scanner;
+
+public class Interpreter {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String line;
+        do {
+            System.out.print("#");
+            line = scanner.nextLine();
+            String[] tokens = line.split(" ");
+            //System.out.println(Arrays.toString(words));
+            switch (tokens[0]){
+                case "print":
+                    printValue(line);
+                    break;
+                case "exit":
+                    System.out.println("exit from Interpreter");
+                    break;
+                default:
+                    assigValue(line);
+            }
+        } while (!line.equals("exit"));
+    }
+    static void assigValue(String line) {
+        String[] tokens = line.split("=");
+        String varName = tokens[0].trim();
+        String varValue = tokens[1].trim();
+    }
+    static void printValue(String line) {
+    }
+}
