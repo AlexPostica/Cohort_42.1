0db8206dee1b35aaf1e2f14aea3270532908abee
AlexPostica <145809454+AlexPostica@users.noreply.github.com>
2024-02-10 19:00:57 +0100

    Update HomeWork4.java

--- src/lesson4/HomeWork4.java
+++ src/lesson4/HomeWork4.java
@@ -8,8 +8,7 @@ import java.util.Scanner;
  * @version 22.Jan
  */
 public class HomeWork4 {
-
-    public static void main(String[] args) {
+     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter your name:");
         String name = scanner.nextLine();
