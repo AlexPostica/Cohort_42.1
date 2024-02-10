c864c823b7fe8437d4fa251ecdb598a20deb06a9
AlexPostica <145809454+AlexPostica@users.noreply.github.com>
2024-02-10 19:01:01 +0100

    Update Lesson4.java

--- src/lesson4/Lesson4.java
+++ src/lesson4/Lesson4.java
@@ -1,9 +1,27 @@
 package lesson4;
+
+import java.util.Scanner;
+
 /**
  * AIT-TR, cohort 42.1, Java Basic, #4
  * @author Alexandru
  * @version 22.Jan
  */
 public class Lesson4 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+//        System.out.println("Enter first number:");
+//        int a = scanner.nextInt();
+//        System.out.println("Enter second number:");
+//        int b = scanner.nextInt();
+//        System.out.println("Result:");
+//        System.out.println(a + " + " + b + " = " + (a + b));
 
+        // class String
+        String str = "Hello, Java";
+        String ch = String.valueOf('a');
+        String[] parts = str.split(" ");
+        String s = 10 + " sm";
+        System.out.println(str.substring(7) + 256 + " is great!");
+    }
 }
