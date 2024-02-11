3d81c840f7fc6e860356c1e00ef43c314724ff60
AlexPostica <145809454+AlexPostica@users.noreply.github.com>
2024-02-11 23:28:06 +0100

    Update HomeWork12.java

--- src/lesson12/HomeWork12.java
+++ src/lesson12/HomeWork12.java
@@ -10,16 +10,36 @@ import java.util.Arrays;
 public class HomeWork12 {
     public static void main(String[] args) {
         // task1
-        int[] arr = {3, 71 ,74, 83, 34, 66, 55, 12};
+        int[] arr = {3, 71, 74, 83, 34, 66, 55, 12};
         int numSearch = 55;
         int idx = -1;
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] == numSearch){
+        int i = 0;
+        int minIndex = i;
+        for (i = 0; i < arr.length; i++) {
+            if (arr[i] == numSearch) {
                 idx = i;
                 break;
             }
         }
         System.out.println(idx);
         System.out.println(Arrays.toString(arr));
+        selectionSort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    // task2
+    public static void selectionSort(int[] arr) {
+        int i;
+        for (i = 0; i < arr.length - 1; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
+                }
+            }
+            int temp = arr[minIndex];
+            arr[minIndex] = arr[i];
+            arr[i] = temp;
+        }
     }
 }
