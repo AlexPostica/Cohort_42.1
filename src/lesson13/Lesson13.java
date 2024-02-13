043ccb6434b04f797373f1d65b9a5a063ac37d8d
AlexPostica <145809454+AlexPostica@users.noreply.github.com>
2024-02-13 19:53:12 +0100

    Update Lesson13.java

--- src/lesson13/Lesson13.java
+++ src/lesson13/Lesson13.java
@@ -1,5 +1,11 @@
 package lesson13;
-
+/**
+ * AIT-TR, cohort 42.1, Java Basic, #12
+ * @author Alexandru
+ * @version 9.Feb
+ */
 public class Lesson13 {
+    public static void main(String[] args) {
 
+    }
 }
