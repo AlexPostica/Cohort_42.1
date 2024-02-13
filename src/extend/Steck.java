8263d9abee84f9cd8b3131f111a0251c7a5e1949
AlexPostica <145809454+AlexPostica@users.noreply.github.com>
2024-02-13 19:52:59 +0100

    Update Steck.java

--- src/extend/Steck.java
+++ src/extend/Steck.java
@@ -1,9 +1,34 @@
 package extend;
+
+import java.util.Arrays;
+
 /**
- * AIT-TR, cohort 42.1, Java Basic, hw #12
+ * AIT-TR, cohort 42.1, Java Basic, #13
  * @author Alexandru
- * @version 9.Feb
+ * @version 12.Feb
  */
 public class Steck {
+    static int[] stack = new int[10];
+    static int idx = 0;
 
+    public static void push(int value) {
+        if (idx < stack.length) {
+            stack[idx] = value;
+            idx++;
+        } else {
+            throw new RuntimeException("Stack overflow");
+        }
+    }
+
+    public static Integer pop() {
+        if (idx > 1) {
+            idx--;
+            return stack[idx];
+        }
+        return null;
+    }
+
+    public static String arrayToString() {
+        return Arrays.toString(Arrays.copyOf(stack, idx));
+    }
 }
