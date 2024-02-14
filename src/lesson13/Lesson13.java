c310558f6c4f1512eaf8402dd5d96a014c0472b7
AlexPostica <145809454+AlexPostica@users.noreply.github.com>
2024-02-14 20:00:55 +0100

    Update Lesson13.java

--- src/lesson13/Lesson13.java
+++ src/lesson13/Lesson13.java
@@ -1,4 +1,8 @@
 package lesson13;
+
+import lesson12.HomeWork12;
+import lesson9.HomeWork9;
+
 /**
  * AIT-TR, cohort 42.1, Java Basic, #12
  * @author Alexandru
@@ -6,6 +10,16 @@ package lesson13;
  */
 public class Lesson13 {
     public static void main(String[] args) {
+        int[] array = HomeWork9.createRandomArray(100_000_000, 100);
 
+        long startTime = System.currentTimeMillis();
+        System.out.println(HomeWork12.linearSearch(array, 88));
+        System.out.println((System.currentTimeMillis() - startTime) + "ms");
+
+        HomeWork12.selectionSort(array);
+
+        startTime = System.currentTimeMillis();
+        System.out.println(HomeWork12.binarySearch(array, 88));
+        System.out.println((System.currentTimeMillis() - startTime) + "ms");
     }
 }
