b00833cf15629d6450870f68bca7baf160d8b294
AlexPostica <145809454+AlexPostica@users.noreply.github.com>
2024-02-14 20:00:43 +0100

    Update Stack.java

--- src/extend/Stack.java
+++ src/extend/Stack.java
@@ -3,25 +3,24 @@ package extend;
 import java.util.Arrays;
 
 /**
- * AIT-TR, cohort 42.1, Java Basic, #13
- * @author Alexandru
- * @version 12.Feb
+ * AIT-TR, cohort 42.1, Java Basic, hw #12 ext
+ *
+ * @author Sergey Iryupin
+ * @version 12-Feb-24
  */
-public class Steck {
-    static int[] stack = new int[10];
+public class Stack {
+    static  int[] stack =new int [10];
     static int idx = 0;
-
-    public static void push(int value) {
-        if (idx < stack.length) {
-            stack[idx] = value;
-            idx++;
+    public static void push(int value){
+        if (idx < stack.length){
+        stack[idx] = value;
+        idx++;
         } else {
             throw new RuntimeException("Stack overflow");
         }
     }
-
-    public static Integer pop() {
-        if (idx > 1) {
+    public static Integer pop(){
+        if (idx > 1){
             idx--;
             return stack[idx];
         }
