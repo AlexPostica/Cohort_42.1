95482b46636456fb1b214eb94adacce018752769
AlexPostica <145809454+AlexPostica@users.noreply.github.com>
2024-02-16 22:14:35 +0100

    Create Cat.java

---
+++ src/lesson15/Cat.java
@@ -0,0 +1,34 @@
+package lesson15;
+
+public class Cat {
+    String name;
+    String color;
+    int age;
+    int volume;
+    int food;
+
+
+    public Cat(String name, String color, int age, int volume) {
+        this.name = name;
+        this.color = color;
+        this.age = age;
+        this.volume = volume;
+    }
+
+    public Cat(String barsik, String black, int i) {
+    }
+
+    public Cat() {
+
+    }
+
+    public void eat(int food) {
+        this.food = food;
+    }
+    public double satieted() {
+        return (double) food / volume * 100;
+    }
+    public void voice() {
+        System.out.println("Meow!");
+    }
+}
