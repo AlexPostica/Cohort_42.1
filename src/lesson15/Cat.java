f0625f4375522c7c3c78e5d49fbfc54f4b3b093a
AlexPostica <145809454+AlexPostica@users.noreply.github.com>
2024-02-18 23:47:52 +0100

    Update Cat.java

--- src/lesson15/Cat.java
+++ src/lesson15/Cat.java
@@ -7,7 +7,6 @@ public class Cat {
     int volume;
     int food;
 
-
     public Cat(String name, String color, int age, int volume) {
         this.name = name;
         this.color = color;
@@ -15,20 +14,36 @@ public class Cat {
         this.volume = volume;
     }
 
-    public Cat(String barsik, String black, int i) {
-    }
-
-    public Cat() {
-
-    }
-
     public void eat(int food) {
         this.food = food;
     }
+
     public double satieted() {
         return (double) food / volume * 100;
     }
+
     public void voice() {
         System.out.println("Meow!");
+    }
+
+    public int walk() {
+        if (food <= 10){
+            System.out.println("I can walk");
+            food -= 10;
+        }else {
+            eat(10);
+            System.out.println("The cat must eat");
+        }
+        return food;
+    }
+    public int run() {
+        if (food <= 20){
+            food -= 20;
+            System.out.println("I can run");
+        } else {
+            eat(20);
+            System.out.println("The cat must eat");
+        }
+        return food;
     }
 }
