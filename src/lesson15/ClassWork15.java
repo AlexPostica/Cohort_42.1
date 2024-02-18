3f6de05701cb3492e49cdc5d0e63f350cac9b71a
AlexPostica <145809454+AlexPostica@users.noreply.github.com>
2024-02-18 23:48:00 +0100

    Update ClassWork15.java

--- src/lesson15/ClassWork15.java
+++ src/lesson15/ClassWork15.java
@@ -6,14 +6,17 @@ package lesson15;
  */
 public class ClassWork15 {
     public static void main(String[] args) {
-        Cat cat = new Cat(); //"Murzik","White", 3
+        Cat cat = new Cat("Mursik", "White", 3, 100);
         cat.voice();
         System.out.println(cat.name +  ", " + cat.color + ", " + cat.age);
-
-        Cat cat1 = new Cat("Barsik", "Black", 5, 10);
+        cat.eat(50);
+        System.out.println(cat.satieted());
+        Cat cat1 = new Cat("Barsik", "Black", 5, 100);
         cat1.voice();
-        cat1.eat(6);
-        System.out.println(cat1.name +  ", " + cat1.color + ", " + cat1.age);
-        System.out.println();
+        System.out.println(cat1.name +  ", " + cat1.color + ", " + "age " + cat1.age);
+        cat1.eat(80);
+        System.out.println(cat1.satieted());
+        System.out.println(cat.walk());
+        System.out.println(cat1.run());
     }
 }
