809549473197d2c14b75569ec458e5f717dd4529
AlexPostica <145809454+AlexPostica@users.noreply.github.com>
2024-02-19 00:02:38 +0100

    Update ClassWork15.java

--- src/lesson15/ClassWork15.java
+++ src/lesson15/ClassWork15.java
@@ -9,14 +9,16 @@ public class ClassWork15 {
         Cat cat = new Cat("Mursik", "White", 3, 100);
         cat.voice();
         System.out.println(cat.name +  ", " + cat.color + ", " + cat.age);
-        cat.eat(50);
+        cat.eat(10);
         System.out.println(cat.satieted());
+        System.out.println(cat.walk());
+        System.out.println();
+
         Cat cat1 = new Cat("Barsik", "Black", 5, 100);
         cat1.voice();
         System.out.println(cat1.name +  ", " + cat1.color + ", " + "age " + cat1.age);
         cat1.eat(80);
         System.out.println(cat1.satieted());
-        System.out.println(cat.walk());
         System.out.println(cat1.run());
     }
 }
