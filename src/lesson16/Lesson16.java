38015564476e04994b6e10e66f0d65d9b13f68b1
AlexPostica <145809454+AlexPostica@users.noreply.github.com>
2024-02-23 18:03:15 +0100

    Update Lesson16.java

--- src/lesson16/Lesson16.java
+++ src/lesson16/Lesson16.java
@@ -6,12 +6,18 @@ package lesson16;
  */
 public class Lesson16 {
     public static void main(String[] args) {
-        Cat cat = new Cat(30);
-        Plate plate = new Plate(50);
-
-        plate.add(40);
+        Cats cat = new Cats(30);
+        Cats[]cats = new Cats[3];
+        cats[0] = new Cats(15);
+        cats[1] = new Cats(20);
+        cats[2] = new Cats(35);
+        Plate plate = new Plate(100);
+        plate.add(80);
         cat.eat(plate);
-
+        for (Cats cats1: cats) {
+            cat.eat(plate);
+            System.out.println("Cat: " + plate.amountFood());
+        }
         System.out.println("Cat: " + cat.realVolume);
         System.out.println("Plate: " + plate.food);
     }
