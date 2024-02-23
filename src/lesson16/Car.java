bfe7925766e10c18b7af787eaf2c3840bbfec3b3
AlexPostica <145809454+AlexPostica@users.noreply.github.com>
2024-02-23 18:00:56 +0100

    Create Car.java

---
+++ src/lesson16/Car.java
@@ -0,0 +1,21 @@
+package lesson16;
+
+import lesson16.HomeWork16.PetrolStation;
+
+public class Car {
+    int maxVolume;
+    public int realVolume;
+
+    public Car(int maxVolume) {
+        this.maxVolume = maxVolume;
+        this.realVolume = 0;
+    }
+    public void refill(PetrolStation petrolStation) {
+        int fuel = petrolStation.amountFuel();
+        if(fuel > maxVolume - realVolume) {
+            fuel = maxVolume - realVolume;
+        }
+        realVolume += fuel;
+        petrolStation.add(fuel);
+    }
+}
