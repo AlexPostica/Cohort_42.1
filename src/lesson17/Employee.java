f7fce610d7a387d17abca455d79accc2724748ff
AlexPostica <145809454+AlexPostica@users.noreply.github.com>
2024-02-23 19:29:29 +0100

    Update Employee.java

--- src/lesson17/Employee.java
+++ src/lesson17/Employee.java
@@ -1,7 +1,7 @@
 package lesson17;
 
 public class Employee {
-    private String fullName;
+    private String Name;
     private String position;
     private String email;
     private String phoneNum;
@@ -9,15 +9,15 @@ public class Employee {
     private int age;
 
     public Employee(String fullName, String position, String email, String phoneNum, double salary, int age) {
-        this.fullName = fullName;
+        this.Name = fullName;
         this.position = position;
         this.email = email;
         this.phoneNum = phoneNum;
         this.salary = salary;
         this.age = age;
     }
-    public String getFullName() {
-        return fullName;
+    public String getName() {
+        return Name;
     }
     public String getPosition() {
         return position;
@@ -33,5 +33,16 @@ public class Employee {
     }
     public int getAge() {
         return age;
+    }
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "fullName='" + Name + '\'' +
+                ", position='" + position + '\'' +
+                ", email='" + email + '\'' +
+                ", phoneNum='" + phoneNum + '\'' +
+                ", salary=" + salary +
+                ", age=" + age +
+                '}';
     }
 }
