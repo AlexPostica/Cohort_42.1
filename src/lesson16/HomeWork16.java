0dfb19bae786de7ad924a1d10f1bffe488d5a2de
AlexPostica <145809454+AlexPostica@users.noreply.github.com>
2024-02-24 19:06:13 +0100

    Create HomeWork16.java

---
+++ src/lesson16/HomeWork16.java
@@ -0,0 +1,11 @@
+package lesson16;
+/**
+ * AIT-TR, cohort 42.1, Java Basic, hw #18
+ * @author Alexandru Postica
+ * @version 23.Feb
+ */
+public class HomeWork16 {
+    public static void main(String[] args) {
+
+    }
+}
