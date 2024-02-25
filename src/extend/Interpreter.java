54d4388185d95ba9ab80190ca6200e1039150420
AlexPostica <145809454+AlexPostica@users.noreply.github.com>
2024-02-25 12:48:03 +0100

    Update Interpreter.java

--- src/extend/Interpreter.java
+++ src/extend/Interpreter.java
@@ -1,37 +1,79 @@
 package extend;
-/**
- * AIT-TR, cohort 42.1, Java Basic, hw #10
- * @Alexandru Postica
- * @version 8-Feb
- */
+
 import java.util.Scanner;
 
+/**
+ * AIT-TR, cohort 42.1, Java Basic, hw #10
+ * @author Alexandru Postica
+ * @ version 8-Feb, 23-Feb
+ */
 public class Interpreter {
+
+    static int[] values = new int[26];
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String line;
         do {
-            System.out.print("#");
+            System.out.print("# ");
             line = scanner.nextLine();
             String[] tokens = line.split(" ");
-            //System.out.println(Arrays.toString(words));
-            switch (tokens[0]){
+            switch (tokens[0]) {
                 case "print":
-                    printValue(line);
+                    printValue(tokens[1]);
                     break;
                 case "exit":
-                    System.out.println("exit from Interpreter");
+                    System.out.println("Exit from interpreter.");
                     break;
                 default:
-                    assigValue(line);
+                    assignValue(line);
             }
         } while (!line.equals("exit"));
     }
-    static void assigValue(String line) {
+
+    static void assignValue(String line) {
         String[] tokens = line.split("=");
         String varName = tokens[0].trim();
         String varValue = tokens[1].trim();
+        // check variable name
+        if (!validateVarName(varName)) {
+            return;
+        }
+        // transform 'a' -> 0
+        int idx = varName.charAt(0) - 'a';
+        // transform "123" -> 123
+        int value = Integer.valueOf(varValue);
+        // assign value
+        values[idx] = value;
     }
-    static void printValue(String line) {
+
+    static void printValue(String varName) {
+        // check variable name
+        if (!validateVarName(varName)) {
+            return;
+        }
+        // transform 'a' -> 0
+        int idx = varName.charAt(0) - 'a';
+        // print value of variable
+        System.out.println(values[idx]);
+    }
+
+    static boolean validateVarName(String varName) {
+        // check variable name length
+        if (varName.length() > 1) {
+            System.out.println("Error: variable name is too long");
+            return true;
+        }
+        if (varName.isEmpty()) {
+            System.out.println("Error: variable name is empty");
+            return true;
+        }
+        // TODO check if variable name in 'a'..'z'
+        char nameChar = varName.charAt(0);
+        if (nameChar < 'a' || nameChar > 'z') {
+            System.out.println("Error: variable name is invalid");
+            return false;
+        }
+        return true;
     }
 }
