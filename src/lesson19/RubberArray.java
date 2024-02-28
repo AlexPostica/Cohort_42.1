e4aefab11836dec272b6d3b4c98c71b8cfad5c93
AlexPostica <145809454+AlexPostica@users.noreply.github.com>
2024-02-28 20:34:40 +0100

    Create RubberArray.java

---
+++ src/lesson19/RubberArray.java
@@ -0,0 +1,57 @@
+package lesson19;
+
+public class RubberArray {
+
+    private final int INIT_DATA_SIZE = 10;
+    private final float EXTEND_INDEX = 1.5f;
+    private int[] data;
+    private int length;
+
+    public RubberArray() {
+            data = new int[INIT_DATA_SIZE];
+            length = 0;
+    }
+
+    public int get(int idx) {
+        return data[idx];
+    }
+
+    public void add(int value) {
+        if (length == data.length) {
+            // create new array
+            int[] newData = new int[(int) (length * EXTEND_INDEX)];
+            for (int i = 0; i < data.length; i++) {
+                newData[i] = data[i];
+            }
+            data = newData;
+        }
+        // add value
+        data[length] = value;
+        length++;
+    }
+
+    public void add(int value, int idx) {
+        //TODO implement
+    }
+
+    public void remove(int idx) {
+        // move element right to left from idx
+        for (int i = idx; i < data.length - 1; i++) {
+            data[i] = data[i + 1];
+        }
+        length--;
+    }
+
+    @Override
+    public String toString() {
+        //TODO use TringBuider instead of String
+        String str = "{";
+        for (int i = 0; i < length; i++) {
+            str += String.valueOf(data[i]);
+            if (i < length - 1) {
+                str += ",";
+            }
+        }
+        return str + "}";
+    }
+}
