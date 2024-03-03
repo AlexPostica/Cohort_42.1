1abc67d392e7177c3474d63e56de60d2fda78556
AlexPostica <145809454+AlexPostica@users.noreply.github.com>
2024-03-03 14:22:49 +0100

    Update HomeWork19.java

--- src/lesson19/homework19/HomeWork19.java
+++ src/lesson19/homework19/HomeWork19.java
@@ -1,15 +1,18 @@
 package lesson19.homework19;
 
-import lesson19.RubberArray;
-
 public class HomeWork19 {
     public static void main(String[] args) {
         RubberArray rb = new RubberArray();
         rb.add(8);
         rb.add(4);
-        rb.add(11);
-        rb.add(6);
-        rb.add(-1);
+        rb.add(3);
+        rb.add(2);
+        rb.add(1);
+        System.out.println(rb);
+        System.out.println(rb.contains(11));
+        System.out.println(rb.indexOf(11));
+        System.out.println(rb.indexOf(9));
+        rb.add(-3 , 3);
         System.out.println(rb);
     }
 }
