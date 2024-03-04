441ef9c911da8a80b1eba55c0e61fa4dac6b3519
AlexPostica <145809454+AlexPostica@users.noreply.github.com>
2024-03-04 22:10:57 +0100

    Create Lesson22.java

---
+++ src/lesson22/Lesson22.java
@@ -0,0 +1,20 @@
+package lesson22;
+
+/**
+ * AIT-TR, cohort 42.1, Java Basic, #22
+ * @author Alexandru Postica
+ * @version 4.Mar
+ */
+public class Lesson22 {
+    public static void main(String[] args) {
+
+        Horse horse = new Horse("Tunder");
+        System.out.println(horse);
+        horse.run();
+
+        Pegas pegas = new Pegas("Pegas");
+        System.out.println(pegas);
+        pegas.run();
+        pegas.fly();
+    }
+}
