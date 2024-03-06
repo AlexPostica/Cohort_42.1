b9d13943de1fdf8fed3030453e9f41cc6f237e5b
AlexPostica <145809454+AlexPostica@users.noreply.github.com>
2024-03-06 21:42:03 +0100

    Create Lesson23.java

---
+++ src/lesson23/Lesson23.java
@@ -0,0 +1,20 @@
+package lesson23;
+
+/**
+ * AIT-TR, cohort 42.1, Java Basic, #22
+ * @author Alexandru Postica
+ * @version 6.Mar
+ */
+public class Lesson23 {
+    public static void main(String[] args) {
+        Lesson23 lesson23 = new Lesson23();
+        System.out.println(lesson23);
+        System.out.println(lesson23.hashCode());
+        System.out.println(Integer.toHexString(lesson23.hashCode()));
+    }
+
+    @Override
+    public String toString() {
+        return "Lesson23{}";
+    }
+}
