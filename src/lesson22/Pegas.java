4772d363ac6a49eb56f383cc6c19c41307013e96
AlexPostica <145809454+AlexPostica@users.noreply.github.com>
2024-03-07 18:51:48 +0100

    Update Pegas.java

--- src/lesson22/Pegas.java
+++ src/lesson22/Pegas.java
@@ -3,13 +3,15 @@ package lesson22;
 public class Pegas extends Horse{
 
     private String wingsCount;
+
     public Pegas(String name) {
         super(name);
     }
 
     public void hidden() {
-        super hidden();
+        super.hidden();
     }
+
     public void fly() {
         System.out.println("fly...");
     }
