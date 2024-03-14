aac9dbe1db20b72fb23319ced38734990e64752d
AlexPostica <145809454+AlexPostica@users.noreply.github.com>
2024-03-14 09:36:25 +0100

    Create HomeWork22.java

---
+++ src/lesson22/homework/HomeWork22.java
@@ -0,0 +1,17 @@
+package lesson22.homework;
+
+public class HomeWork22 {
+    public static void main(String[] args) {
+        Animal animal = new Animal(5);
+        System.out.println(animal);
+        System.out.println(animal.voice());
+
+        Cat cat = new Cat(3, "White");
+        System.out.println(cat);
+        System.out.println(cat.voice());
+
+        Tiger tiger = new Tiger(4,"Orange", "Diego");
+        System.out.println(tiger);
+        System.out.println(tiger.voice());
+    }
+}
