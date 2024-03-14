11884bc8337444571376693371b28e9400ce12ac
AlexPostica <145809454+AlexPostica@users.noreply.github.com>
2024-03-14 09:36:37 +0100

    Create HomeWork23.java

---
+++ src/lesson23/homework23/HomeWork23.java
@@ -0,0 +1,11 @@
+package lesson23.homework23;
+/**
+ * AIT-TR, cohort 42.1, Java Basic, #23
+ * @author Alexandru Postica
+ * @version 13.Mar
+ */
+public class HomeWork23 {
+    public static void main(String[] args) {
+
+    }
+}
