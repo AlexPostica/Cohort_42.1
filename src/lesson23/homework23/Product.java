58ea26ab2fd721cb74bd86869a93335de038296d
AlexPostica <145809454+AlexPostica@users.noreply.github.com>
2024-03-14 09:36:44 +0100

    Create Product.java

---
+++ src/lesson23/homework23/Product.java
@@ -0,0 +1,11 @@
+package lesson23.homework23;
+
+public class Product {
+    private  String name;
+    private double price;
+
+    public Product(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
+}
