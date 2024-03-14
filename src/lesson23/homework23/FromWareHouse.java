a4aab700cfd6ddd6d0a25e6fb98c0b219947c045
AlexPostica <145809454+AlexPostica@users.noreply.github.com>
2024-03-14 09:36:34 +0100

    Create FromWareHouse.java

---
+++ src/lesson23/homework23/FromWareHouse.java
@@ -0,0 +1,12 @@
+//package lesson23.homework23;
+//
+//public class FromWareHouse extends BaseDocument {
+//
+//    public FromWareHouse(int id) {
+//        super(id);
+//    }
+//
+//    public void toWareHouse(Oder oder) {
+//        products = oder.getProducts;
+//    }
+//}
