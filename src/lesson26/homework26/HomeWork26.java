a373318cda614dd9d4b12a621f4579e33524c7c7
AlexPostica <145809454+AlexPostica@users.noreply.github.com>
2024-03-15 19:52:18 +0100

    Update HomeWork26.java

--- src/lesson26/homework26/HomeWork26.java
+++ src/lesson26/homework26/HomeWork26.java
@@ -2,12 +2,12 @@ package lesson26.homework26;
 
 public class HomeWork26 {
     public static void main(String[] args) {
-        Figura[] figuras = {new Circle(5, 5),
+        Figura[] figuras = {new Circle(5),
                 new Rectangle(5, 4,4),
                 new Triangle(5,5,5)};
         for (Figura figura: figuras) {
-            System.out.println(figura.Area());
-            System.out.println(figura.Perimeter());
+            System.out.println(figura.calcArea());
+            System.out.println(figura.calcPerimeter());
         }
     }
 }
