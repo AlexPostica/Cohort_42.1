5e4674074cc97760b2fff32709c23f1e318a019a
AlexPostica <145809454+AlexPostica@users.noreply.github.com>
2024-03-15 19:52:21 +0100

    Update Triangle.java

--- src/lesson26/homework26/Triangle.java
+++ src/lesson26/homework26/Triangle.java
@@ -3,20 +3,23 @@ package lesson26.homework26;
 public class Triangle extends Figura{
     private final double side1;
     private final double side2;
+    private final double side3;
 
-    public Triangle(int side, double side1, double side2) {
-        super(side);
+
+    public Triangle(int side1, double side2, double side3) {
         this.side1 = side1;
         this.side2 = side2;
+        this.side3 = side3;
     }
 
     @Override
-    public double Area() {
-        return (side1 * side2 * side)/2;
+    public double calcArea() {
+        double s = calcPerimeter()/2;
+        return Math.sqrt(s*(s-side1)*(s-side2)*(s-side3));
     }
 
     @Override
-    public double Perimeter() {
-        return side1 + side2 + side;
+    public double calcPerimeter() {
+        return side1 + side2 + side3;
     }
 }
