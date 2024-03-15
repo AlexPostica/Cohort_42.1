587b58437926ba6abc8f42b47bcd9795685432c6
AlexPostica <145809454+AlexPostica@users.noreply.github.com>
2024-03-15 14:16:58 +0100

    Update Homework25.java

--- src/lesson25/Homework25/Homework25.java
+++ src/lesson25/Homework25/Homework25.java
@@ -1,15 +1,30 @@
 package lesson25.Homework25;
 
+/**
+ * AIT-TR, cohort 42.1, Java Basic, #25
+ * @author Alexandru Postica
+ * @version 13.Mar
+ */
 public class Homework25 {
     public static void main(String[] args) {
-        Human human = new Human(10, 15);
-        System.out.println(human.run());
-        System.out.println(human.rest());
-        NoProfRun noProfRun = new NoProfRun(15,10);
-        System.out.println(noProfRun.run());
-        System.out.println(noProfRun.rest());
-        ProfRun profRun = new ProfRun(20,5);
-        System.out.println(profRun.run());
-        System.out.println(profRun.rest());
+
+        // task #1
+        Human[] humans = {new Human(10, 15), new NoProfRun(15,10),new NoProfRun(15,10),new ProfRun(20,5)};
+        for (Human human: humans) {
+            System.out.println(human.run());
+            System.out.println(human.rest());
+        }
+
+        // task #2
+        Vehicle[] vehicles = {new Vehicle(), new Becycle(), new Car()};
+            for (Vehicle vehicle: vehicles) {
+                System.out.println(vehicle.startEngine());
+        }
+
+        // task #3
+        Figura[] figuras = {new Figura(), new Squere(5), new Circle(5), new Triangle(5)};
+            for (Figura figura: figuras) {
+                System.out.println(figura.calcArea());
+            }
     }
 }
