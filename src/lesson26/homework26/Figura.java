9f03e931379134bf827adb97cf71d174845cbd7e
AlexPostica <145809454+AlexPostica@users.noreply.github.com>
2024-03-15 19:52:12 +0100

    Update Figura.java

--- src/lesson26/homework26/Figura.java
+++ src/lesson26/homework26/Figura.java
@@ -1,12 +1,6 @@
 package lesson26.homework26;
 
 public abstract class Figura {
-    protected int side;
-
-    public Figura(int side) {
-        this.side = side;
-    }
-
-    public abstract double Area();
-    public abstract double Perimeter();
+    public abstract double calcArea();
+    public abstract double calcPerimeter();
 }
