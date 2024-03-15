a3ae5e9690058393aef44c7b7bc08565d9240ab2
AlexPostica <145809454+AlexPostica@users.noreply.github.com>
2024-03-15 14:17:04 +0100

    Update NoProfRun.java

--- src/lesson25/Homework25/NoProfRun.java
+++ src/lesson25/Homework25/NoProfRun.java
@@ -1,6 +1,6 @@
 package lesson25.Homework25;
 
-public class NoProfRun extends Human{
+public class NoProfRun extends Human {
     public NoProfRun(int speed, int rest) {
         super(speed, rest);
     }
