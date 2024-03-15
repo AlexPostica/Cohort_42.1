e85bea20d043995a8a1907401ea3bec759d3968c
AlexPostica <145809454+AlexPostica@users.noreply.github.com>
2024-03-15 14:17:33 +0100

    Create Lesson26.java

---
+++ src/lesson26/Lesson26.java
@@ -0,0 +1,12 @@
+package lesson26;
+/**
+ * AIT-TR, cohort 42.1, Java Basic, #26
+ * @author Alexandru Postica
+ * @version 6.Mar
+ */
+public class Lesson26 {
+    public static void main(String[] args) {
+
+    }
+}
+
