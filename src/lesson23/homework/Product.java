c146678b16fa2b8cb9504ee1c30e1dab92cef563
AlexPostica <145809454+AlexPostica@users.noreply.github.com>
2024-03-21 21:43:03 +0100

    Create Product.java

---
+++ src/lesson23/homework/Product.java
@@ -0,0 +1,18 @@
+
+package lesson23.homework;
+
+public class Product {
+    private int id;
+    private String name;
+    private double price;
+
+    public Product(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "{" + name + ", " + price + '}';
+    }
+}
