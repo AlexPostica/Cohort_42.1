99494c2427c45050559020507eb254c1883447e4
AlexPostica <145809454+AlexPostica@users.noreply.github.com>
2024-03-23 10:24:26 +0100

    Update ToWareHouse.java

--- src/lesson23/homework/ToWareHouse.java
+++ src/lesson23/homework/ToWareHouse.java
@@ -1,33 +1,11 @@
-
 package lesson23.homework;
 
-public class ProductAndCount {
-    private Product product;
-    private int count;
-
-    public ProductAndCount(Product product, int count) {
-        this.product = product;
-        this.count = count;
+public class ToWarehouse extends BaseDocument {
+    public ToWarehouse(int id) {
+        super(id);
     }
 
-    public Product getProduct() {
-        return product;
-    }
-
-    public void setProduct(Product product) {
-        this.product = product;
-    }
-
-    public int getCount() {
-        return count;
-    }
-
-    public void setCount(int count) {
-        this.count = count;
-    }
-
-    @Override
-    public String toString() {
-        return "{" + product + ", " + count + '}';
+    public void toWarehouse(WareHouse warehouse) {
+        warehouse.addAll(products);
     }
 }
