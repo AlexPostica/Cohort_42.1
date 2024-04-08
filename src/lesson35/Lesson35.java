4c1d71851010156c8d1786037fea1bad9e69914f
AlexPostica <145809454+AlexPostica@users.noreply.github.com>
2024-04-08 20:37:53 +0200

    Update Lesson35.java

--- src/lesson35/Lesson35.java
+++ src/lesson35/Lesson35.java
@@ -5,6 +5,11 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+/**
+ * AIT-TR, cohort 42.1, Java Basic, #35
+ * @author Alexandru Postica
+ * @version 8.Apr
+ */
 public class Lesson35 {
     public static void main(String[] args) {
         List<String> stringList = new ArrayList<>(List.of("i", "love", "Java"));
